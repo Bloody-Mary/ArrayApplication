ed7224b2762872a35a52e77e64e29cff116fbe25
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-07-04 21:27:07 +0300

    Create AscendingArray

---
+++ src/main/java/ru/babushkina/week7/add_task/ascendsortarray/_main/AscendingArray.java
@@ -0,0 +1,36 @@
+package ru.babushkina.week7.add_task.ascendsortarray._main;
+
+import java.util.Arrays;
+
+public class AscendingArray {
+    public static void main(String[] args) {
+        int[] arr = generateArray();
+        String arrayAsString = Arrays.toString(arr);
+        int[] sortArray = sortedArray(arr);
+
+        System.out.println(arrayAsString);
+        System.out.println(Arrays.toString(sortArray));
+    }
+
+    public static int[] generateArray() {
+        int[] array = new int[4];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 21) - 10;
+        }
+        return array;
+    }
+
+    public static int[] sortedArray(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
